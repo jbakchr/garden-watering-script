94d6424d93bb92143945fe96645cfcaf97b28992
Jonas Bak Phillipson <jbakchr@gmail.com>
2022-04-03 19:38:47 +0200

    Corrected date_number if check and added initial print for watering condition

--- app.py
+++ app.py
@@ -7,16 +7,14 @@ year = datetime.now().strftime("%Y")
 month = datetime.now().strftime("%B")
 date_number = datetime.now().day
 
-
 url = f"https://www.dmi.dk/dmidk_obsWS/rest/archive/daily/danmark/precip/{city}/{year}/{month}"
 r = requests.get(url)
 data = json.loads(r.text)
 
-# print(data["dataserie"][2])
-
 rain_amount = 0
-if date_number >= 2:
+if date_number >= 3:
     for i in range(date_number - 1, (date_number - 3), -1):
         rain_amount += data["dataserie"][i]["valueRounded2OneDecimal"]
 
-print(rain_amount)
+if rain_amount < 1.0:
+    print("Perhaps you should water the garden ..")
