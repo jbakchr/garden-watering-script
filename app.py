a33eba694d74ff56e79c51d631e8fede054ab460
Jonas Bak Phillipson <jbakchr@gmail.com>
2022-04-09 20:39:04 +0200

    Added tkinter instead of pyautogui

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 import json
+import tkinter as tk
+from tkinter import ttk
+
 import requests
 import pyautogui
 
@@ -17,6 +20,15 @@ if date_number >= 3:
     for i in range(date_number - 1, (date_number - 3), -1):
         rain_amount += data["dataserie"][i]["valueRounded2OneDecimal"]
 
-if rain_amount < 1.0:
-    pyautogui.confirm(text="Perhaps you should water to garden ..",
-                      title="Awesome Garden Watering App", buttons=['OK'])
+if rain_amount > 1.0:
+    root = tk.Tk()
+
+    ttk.Label(root, text="Perhaps you should water the garden",
+              padding=(20, 20)).pack()
+
+    ok_button = ttk.Button(root, text="OK", command=root.destroy)
+    ok_button.pack()
+
+    root.mainloop()
+    # pyautogui.confirm(text="Perhaps you should water to garden ..",
+    #   title="Awesome Garden Watering App", buttons=['OK'])
